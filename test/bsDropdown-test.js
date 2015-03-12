25463d0ada59b554eb0ab57df6f281b585f6cc1a
AllenFang <ayu780129@hotmail.com>
2015-03-12 22:41:16 +0800

    add divider test for bsDropdown.

--- test/bsDropdown-test.js
+++ test/bsDropdown-test.js
@@ -2,6 +2,14 @@ describe("bsDropdown Tests", function(){
 	var mockScope;
 	var compile;
 	var el;
+	var dropdownItem = [
+		"Option0",
+		"Option1",
+		"Option2",
+		"Option3",
+		"Option4",
+		"Option5"
+	];
 
 	beforeEach(angular.mock.module("ng.bs.dropdown"));
 
@@ -9,14 +17,7 @@ describe("bsDropdown Tests", function(){
 		beforeEach(inject(function($rootScope, $compile){
 			mockScope = $rootScope.$new();
 			compile = $compile;
-			mockScope.data = [
-				"Option0",
-				"Option1",
-				"Option2",
-				"Option3",
-				"Option4",
-				"Option5"
-			];
+			mockScope.data = dropdownItem;
 			//default select value
 			mockScope.selectData = mockScope.data[3];
 
@@ -66,6 +67,68 @@ describe("bsDropdown Tests", function(){
 			for(var i=0;i<itemElms.length;i++){
 				itemElms.eq(i).find("a").triggerHandler("click");
 				expect(mockScope.selectData).toBe(mockScope.data[i]);
+			}
+		});
+	});
+
+	describe("A dropdown with divider test", function(){
+		var _newDividerPos;
+		beforeEach(inject(function($rootScope, $compile){
+			mockScope = $rootScope.$new();
+			compile = $compile;
+			mockScope.data = dropdownItem;
+			//default select value
+			mockScope.selectData = null;
+
+			el = "<div bs-dropdown bs-dropdown-items='data' bs-dropdown-divider='{{[1,3]}}' ng-model='selectData'></div>";
+			el = $compile(el)(mockScope);
+			mockScope.$digest();
+		}));
+
+		it("the isolateScope in bsDropdown directive should be correct", function(){
+			var isolated = el.isolateScope();
+			expect(isolated.divider).toBeDefined();
+			expect(isolated.divider.length).toBe(2);
+
+			var _bsDropdownItems = isolated._bsDropdownItems;
+			expect(_bsDropdownItems).not.toBeNull();
+			expect(_bsDropdownItems).toBeDefined();
+			expect(mockScope.data.length+isolated.divider.length).toBe(_bsDropdownItems.length);		
+			
+			var offset = 0;
+			_newDividerPos = [];
+			var pos = 1;
+			for(var i=0;i<isolated.divider.length;i++){
+				_newDividerPos.push(isolated.divider[i]+pos);
+				pos++;
+			}
+			for(var i=0;i<_bsDropdownItems.length;i++){
+				var item = _bsDropdownItems[i];
+				expect(item).toBeDefined();
+				expect(item._k).toBe(i);
+				expect(item.isDisabled).toBe(false);
+
+				if(_newDividerPos.indexOf(i) != -1){
+					expect(item.text).toBe(null);
+					expect(item.isDivider).toBe(true);
+					offset++;
+				}else{
+					expect(item.text).toBe(mockScope.data[i-offset]);
+					expect(item.isDivider).toBe(false);
+				}
+			}
+		});
+
+		it("the item on dropdown should be correct", function(){
+			var itemElms = el.find("li");
+			var offset = 0;
+			for(var i=0;i<itemElms.length;i++){
+				if(_newDividerPos.indexOf(i) != -1){
+					offset++;
+					expect(itemElms.eq(i).attr("class").indexOf('divider')).not.toBe(-1);
+				}else{
+					expect(itemElms.eq(i).find("a").text()).toBe(mockScope.data[i-offset]);
+				}
 			}
 		});
 	});
